91432f84a4775b9071cb46e6187f5bc3dee6c364
shamchon <cx15@duke.edu>
2014-11-10 23:05:11 -0500

    Diners constructed

--- mysite/add.py
+++ mysite/add.py
@@ -6,6 +6,9 @@ from express.models import *
 
 diners = [
     Diner(name="Blue Express", loc="LSRC"),
+    Diner(name="Twinnie's", loc="CIEMAS"),
+    Diner(name="Loop Pizza Grill", loc="Bryan Center"),
+    Diner(name="Au Bon Pain", loc="Bryan Center"),
 ]
 
 for diner in diners:
