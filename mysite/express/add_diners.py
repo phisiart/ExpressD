b80d71ffb223f0c09c38fff446604143b9c05e84
Johnson Tan <phisiart@gmail.com>
2014-11-10 22:29:06 -0500

    add first diner

---
+++ mysite/express/add_diners.py
@@ -0,0 +1,3 @@
+from models import *
+
+# diner = Diner(name="Au Bon Pain", loc="Bryan Center")
--- mysite/express/admin.py
+++ mysite/express/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from express.models import Diner
 
 # Register your models here.
+admin.site.register(Diner)
