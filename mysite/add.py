1bea73711a1540444924fb1398a686ba17b17fae
Johnson Tan <phisiart@gmail.com>
2014-11-11 22:33:08 -0500

    addmenu

--- mysite/add.py
+++ mysite/add.py
@@ -1,8 +1,11 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+import django
+django.setup()
 
 from express.models import *
 
+Diner.objects.all().delete()
 
 diners = [
     Diner(name="Blue Express", loc="LSRC"),
